54b198483f7a951d53ebad06a9d99fee2e23adc0
zuravskm <marketa.zuravska@seznam.cz>
2019-11-10 10:55:22 +0100

    chybové hlášky ve výpočtu Marinova zobrazení

--- ukol_1/du1.py
+++ ukol_1/du1.py
@@ -1,4 +1,5 @@
 # výpočet Marinova zobrazení pro volitelný poloměr Země a měřítko
+# ošetření chybových hlášek při překročení vzdálenosti 1 m mezi přímkami sítě souřadnic
 
 r = float(input("Zadej poloměr Země v km (s desetinnou tečkou):"))
 m = int(input("Zadej měřítko (z tvaru 1:m zadej pouze číslo m):")) # to je v cm
@@ -13,7 +14,10 @@ seznam_rovnobezky = []
 from math import pi
 for v in range(-90,100,10):
     x = (round(((r*(((v* 2)/360)*pi)/m)*100000),1)) #vynásobeno 100000 pro převod na cm a zaokrouhleno na 1 des. místo
-    seznam_rovnobezky.append(x)
+    if x <= -100.0 or x >= 100.0:
+        seznam_rovnobezky.append("-")
+    else:
+        seznam_rovnobezky.append(x)
 print("Rovnoběžky:", seznam_rovnobezky)
 
 #u je zeměpisná šířka
@@ -22,5 +26,8 @@ y = float()
 seznam_poledniky = []
 for u in range(-180,190,10):
     y = (round(((r*(((u*2)/360)*pi)/m)*100000), 1))
-    seznam_poledniky.append(y)
+    if y <= -100.0 or y >= 100.0:
+        seznam_poledniky.append("-")
+    else:
+        seznam_poledniky.append(y)
 print("Poledníky:", seznam_poledniky)
