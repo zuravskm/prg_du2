f54301099262264200c64483afc853d92d241e4a
zuravskm <marketa.zuravska@seznam.cz>
2019-11-12 22:56:32 +0100

    pozžití funkce radians místo složitého převádění stupňů na radiány

--- ukol_1/du1.py
+++ ukol_1/du1.py
@@ -3,13 +3,9 @@
 # ošetření chybových hlášek při překročení vzdálenosti 1 m mezi přímkami sítě souřadnic
 # ošetření chybových hlášek při zadání špatného písmene zobrazení, poloměru a měřítka
 
+from math import radians, pi, sin, tan
+
 z = input("Zadej zobrazení:")
-while z =="":
-    if z == "A" or z == "L" or z == "B":
-        print("Zobrazení zadáno správně")
-    else pass:
-        print("Zadej správné písmeno zobrazení!")
-        break
 r = float(input("Zadej poloměr Země v km (s desetinnou tečkou):"))
 while r <= 0:
     print("Zadej správně poloměr Země!")
@@ -23,9 +19,8 @@ y = float()
 v = int() # v je zeměpisná délka
 x = float()
 seznam_poledniky = [] # výpočet poledníků je shodný pro všechna zobrazení
-from math import pi
 for v in range(-180, 190, 10):
-    x = (round(((r * (((v * 2) / 360) * pi) / m) * 100000), 1))
+    x = (round(((r*(((v* 2)/360)*pi)/m)*100000),1))
     if x <= -100.0 or x >= 100.0:
         seznam_poledniky.append("-")
     else:
@@ -33,9 +28,8 @@ for v in range(-180, 190, 10):
 while z != "x":
     if z == "A":
         seznam_rovnobezky = []
-        from math import pi
         for u in range(-90, 100, 10):
-            y = (round(((r * (((u * 2) / 360) * pi) / m) * 100000), 1))
+            y = (round(((r*((radians(u)))/m)*100000), 1))
             # vynásobeno 100000 pro převod na cm a zaokrouhleno na 1 des. místo
             if y <= -100.0 or y >= 100.0:
                 seznam_rovnobezky.append("-")
@@ -44,9 +38,8 @@ while z != "x":
         break
     elif z == "L":
         seznam_rovnobezky = []
-        from math import pi, sin
         for u in range(-90, 100, 10):
-            y = (round(((r * (sin((u * 2) / 360)) / m) * 100000), 1))
+            y = (round(((r*(sin(radians(u)))/m)*100000), 1))
             if y <= -100.0 or y >= 100.0:
                 seznam_rovnobezky.append("-")
             else:
@@ -54,9 +47,8 @@ while z != "x":
         break
     elif z == "B":
         seznam_rovnobezky = []
-        from math import pi, tan
         for u in range(-80, 90, 10):  # fce tan není definována v 90°
-            y = (round(((r * (tan((((u * 2) / 360) * pi) / 2)) / m) * 100000), 1))
+            y = (round(((r*(tan(((radians(u)))/2))/m)*100000), 1))
             if y <= -100.0 or y >= 100.0:
                 seznam_rovnobezky.append("-")
             else:
