adb3549d8d78232d2e46df3d5f3f2fa4089a33dd
zuravskm <marketa.zuravska@seznam.cz>
2019-11-14 17:47:01 +0100

    nic moc novýho

--- ukol_1/du1.py
+++ ukol_1/du1.py
@@ -1,27 +1,29 @@
-# sloučení výpočtu Marinova, Lambertova a Braunova zobrazení
+# sloučení výpočtu Marinova, Lambertova, Braunova a Mercartorova zobrazení
 # volitelný poloměr Země a měřítko
 # ošetření chybových hlášek při překročení vzdálenosti 1 m mezi přímkami sítě souřadnic
 # ošetření chybových hlášek při zadání špatného písmene zobrazení, poloměru a měřítka
 
 from math import radians, pi, sin, tan, log
 
-z = input("Zadej zobrazení:")
+z = input("Zadej počáteční písmeno zobrazení:")
 r = float(input("Zadej poloměr Země v km (s desetinnou tečkou):"))
 m = int(input("Zadej měřítko (z tvaru 1:m zadej pouze číslo m):"))
 u = int() # u je zeměpisná šířka
 y = float()
 v = int() # v je zeměpisná délka
 x = float()
-seznam_poledniky = [] # výpočet poledníků je shodný pro všechna zobrazení
-for v in range(-180, 190, 10):
+seznam_poledniky = []
+seznam_rovnobezky = []
+
+for v in range(-180, 190, 10): # výpočet poledníků je shodný pro všechna zobrazení
     x = (round(((r*(((v* 2)/360)*pi)/m)*100000),1))
     if x <= -100.0 or x >= 100.0:
         seznam_poledniky.append("-")
     else:
         seznam_poledniky.append(x)
+
 while z != "x":
     if z == "A":
-        seznam_rovnobezky = []
         for u in range(-90, 100, 10):
             y = (round(((r*((radians(u)))/m)*100000), 1))
             # vynásobeno 100000 pro převod na cm a zaokrouhleno na 1 des. místo
@@ -31,7 +33,6 @@ while z != "x":
                 seznam_rovnobezky.append(y)
         break
     elif z == "L":
-        seznam_rovnobezky = []
         for u in range(-90, 100, 10):
             y = (round(((r*(sin(radians(u)))/m)*100000), 1))
             if y <= -100.0 or y >= 100.0:
@@ -40,7 +41,6 @@ while z != "x":
                 seznam_rovnobezky.append(y)
         break
     elif z == "B":
-        seznam_rovnobezky = []
         for u in range(-80, 90, 10):  # fce tan není definována v 90°
             y = (round(((r*(tan(((radians(u)))/2))/m)*100000), 1))
             if y <= -100.0 or y >= 100.0:
@@ -49,14 +49,14 @@ while z != "x":
                 seznam_rovnobezky.append(y)
         break
     elif z == "M":
-        seznam_rovnobezky = []
         for u in range(-80, 90, 10):
-            y = (round(((r*(log(1/(tan(radians((90-u)/2)))))/m)*1000), 1))
+            y = (round(((r*(log(1/(tan(radians((90-u)/2)))))/m)*100000), 1))
             if y <= -100.0 or y >= 100.0:
                 seznam_rovnobezky.append("-")
             else:
                 seznam_rovnobezky.append(y)
         break
+
 print("Zadané zobrazení:", z)
 print("Zadaný poloměr Země je:", r, "km")
 print("Zadané měřítko je: 1 :", m)
