71f845631cb526947883a8204e74eb6cb212bc40
zuravskm <marketa.zuravska@seznam.cz>
2019-12-13 21:45:03 +0100

    layout of individual  units of program

--- ukol_2/split.py
+++ ukol_2/split.py
@@ -1,7 +1,12 @@
 import json
 
+### loading input GeoJSON data
+
 with open("input.geojson", "r", encoding="utf-8") as f:
     data = json.load(f)
+
+
+### find and save coordinates to a new list
 
 # print(data)
 # print(len(data['features']))
@@ -18,3 +23,21 @@ for feat in data["features"][0]["geometry"]:
 print(coordinates)
 print(len(coordinates))
 
+
+###sorting_animate data by coordinates
+
+
+
+
+### distribution data by quadtree
+
+
+
+
+### finally saving the output to a new GoeJSON file
+
+# gj_structure = {'type': 'FeatureCollection'}
+# gj_structure['features'] = something
+
+# with open("output.geojson", "w", encoding="utf-8") as f:
+    # json.dump(gj_structure, f, indent=2, ensure_ascii=False)
