c696a970aed97fffa2c95f1e03b65fd735198542
zuravskm <marketa.zuravska@seznam.cz>
2019-12-13 21:25:52 +0100

    pokus commit

--- ukol_2/split.py
+++ ukol_2/split.py
@@ -3,8 +3,18 @@ import json
 with open("input.geojson", "r", encoding="utf-8") as f:
     data = json.load(f)
 
-print(data)
-print(len(data['features']))
+# print(data)
+# print(len(data['features']))
 
-feat = data["features"]
-print(feat)
+# feat = data["features"][0]["geometry"]
+# print(feat)
+
+coordinates = []
+
+for feat in data["features"][0]["geometry"]:
+    if feat["coordinates"] == :
+        coordinates.append(feat)
+
+print(coordinates)
+print(len(coordinates))
+
